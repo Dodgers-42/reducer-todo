4c901271cfe046ef43e91110add6b91545104b27
Dodger-42 <kevin.bing.barber@gmail.com>
2020-08-21 17:02:00 -0700

    yep

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -7,7 +7,7 @@ export default function Todo({ todo, dispatch }) {
             <span style={{ color: todo.complete ? '#AAA' : '#000'}} >
                 {todo.name}
             </span>
-            <button onClick= {() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id }})}>Toggle</button>
+            <button onClick= {() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id }})}>Mark Done</button>
             <button onClick= {() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id }})}>Clear Completed</button>
         </div>
   )
